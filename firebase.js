25327838b466c85b5cf64c54d6337e85391f9fd8
asife23 <161197355+asife23@users.noreply.github.com>
2026-02-21 07:20:20 +0600

    Update Firebase config with new project settings
    Updated Firebase configuration with new project details.

--- firebase.js
+++ firebase.js
@@ -1,13 +1,16 @@
 // Firebase Config
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDxwY4XLnqJ-LH1MyUifHHm7wuoUqpd35g",
+  authDomain: "shop-genesis-bd.firebaseapp.com",
+  projectId: "shop-genesis-bd",
+  storageBucket: "shop-genesis-bd.firebasestorage.app",
+  messagingSenderId: "55271122517",
+  appId: "1:55271122517:web:6e4bf76558f6dbfd2996dc",
+  measurementId: "G-G6T3F6JG1R"
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+// Initialize Firestore
 const db = firebase.firestore();
